dcb53eebe53c8147ba2ae54cb3db4774103c37cc
Песецкий Никита Андреевич <pesetsky2003g@gmail.com>
2022-07-17 11:03:48 +0300

    did task 2, 3

--- LR5/script.js
+++ LR5/script.js
@@ -1,3 +1,4 @@
+//задание 1
 /*
 хрень которую написал я
 function nodecheck(node, numberUL, numberLI = 1){
@@ -24,7 +25,31 @@ for (let node of document.body.childNodes) {
 */
 
 //то что надо
+//задание 1
 console.log(document.all)
 for (let item of document.all){
   console.log(item);
 }
+
+//заданеи 2
+console.log(document.body)
+for (let item of document.body.childNodes){
+  if(item.children)console.log(item);
+}
+
+//заданеи 3
+//первый вариант
+for (let item of document.all){
+  if(item.tagName == "SPAN"){//сравнение тега с словом SPAN с помощью tagName
+    console.log(item.innerHTML);//вынимаем содержимое с помощью innerHTML
+    break;
+  }
+}
+//второй вариант
+let spanitem = document.getElementsByTagName("SPAN")
+console.log(spanitem.item(Date).innerHTML)
+//третий способ
+console.log(document.getElementById('identspan').innerHTML)
+
+
+//задание 4
