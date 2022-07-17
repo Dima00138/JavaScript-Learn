c122f21285461c94bd4d81605e894c30cb044590
Песецкий Никита Андреевич <pesetsky2003g@gmail.com>
2022-07-17 10:18:00 +0300

    did task 1 and 2

---
+++ LR5/script.js
@@ -0,0 +1,21 @@
+// задание 1 и 2
+function nodecheck(node, numberUL, numberLI = 1){
+  if(node.children) { //проверка на наличие содержимого у элемента коллекции
+    for(let item of node.children){ // перебор содержимого
+      console.log(numberUL + "." + numberLI);
+      console.log(item);
+      if(item.children){ //проверка на наличие у содержимого собственного содержимого
+        numberLI++;
+        nodecheck(item, numberUL, numberLI); //рекурсия
+      }
+    }
+  }
+  return;
+}
+
+let number = 1;
+for (let node of document.body.childNodes) {
+  if(node.children)console.log(node);// покажет узел из коллекции
+  nodecheck(node, number);
+    if(node.children)number++;
+}
