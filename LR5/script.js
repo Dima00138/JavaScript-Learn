0e74573f0eaeffb2a5118f29d8ba38f13c90061f
Песецкий Никита Андреевич <pesetsky2003g@gmail.com>
2022-07-17 10:21:42 +0300

    Update script.js

--- LR5/script.js
+++ LR5/script.js
@@ -1,4 +1,5 @@
-// задание 1 и 2
+/*
+хрень которую написал я
 function nodecheck(node, numberUL, numberLI = 1){
   if(node.children) { //проверка на наличие содержимого у элемента коллекции
     for(let item of node.children){ // перебор содержимого
@@ -18,4 +19,12 @@ for (let node of document.body.childNodes) {
   if(node.children)console.log(node);// покажет узел из коллекции
   nodecheck(node, number);
     if(node.children)number++;
+}
+
+*/
+
+//то что надо
+console.log(document.all)
+for (let item of document.all){
+  console.log(item);
 }
